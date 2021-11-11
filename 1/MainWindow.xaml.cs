48fc7f94415c61397d5b6976e33371e3e5bc7067
исп-31 <-31@LAB17-03>
2021-11-11 15:20:05 +0300

    Добавьте файлы проекта.

---
+++ 1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClassLibrary1;
+
+namespace _1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнила студентка группы ИСП-31 Баева Ирина\n" +
+                "Генерировать случайные числа Х, распределенные в диапазоне от - 4 до 7 и вычислять для чисел > 0 " +
+                "X, а для чисел < 0 функцию x^2.Вычисления прекратить, когда подряд появится два одинаковых случайных числа." +
+                "На экран необходимо выводить сгенерированное число и результат расчета функции наразных строках");
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            int n = random.Next(10);
+            Class1.Start(n, out string str, out string str2 , out string str3); //используем функцию
+            Zn.Text = str; //вывод сгенерированных чисел
+            Rez1.Text = Convert.ToString(str2); //вывод str2(выводим результаты чисел)
+            Rez2.Text = Convert.ToString(str3); //вывод str3(выводим результаты чисел)
+        } 
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="n"></param>
+		/// <param name="str"></param>
+		/// <param name="rez1"></param>
+		/// <param name="rez2"></param>
+		public static void Start(int n, out string str, out string str2,out string str3)
+		{
+			str = "";// строка с генерирированнными числами
+			str2 = "";// строка с результатами чисел
+			str3 = "";// строка с результатами чисел
+			double rez1 = 0; //результат rez1
+			double rez2 = 0; //результат rez2
+			int zn = 0; // число
+			Random rnd = new Random();
+			for (int i = 0; i < n; i++)
+			{
+				//диапазон от -4 до 7
+				zn = rnd.Next(-4, 7);
+				if (zn > 0)
+				{
+					rez1 = Math.Sqrt(zn); //вычисляем результат
+					rez1 = Math.Round(rez1);//округляем результат числа
+					str2 += Convert.ToString(rez1) + " , ";//заполнение строки с результатами чисел
+				}
+				else
+				{
+					if (zn < 0)
+					{
+						rez2 = zn * zn;//вычисляем результат
+						str3 += Convert.ToString(rez2) + " , ";//заполнение строки с результатами чисел
+					}
+				}
+				str += Convert.ToString(zn) + " , "; //заполнение строки с геннерированными числами
+			}
+		}
+	}
+}
